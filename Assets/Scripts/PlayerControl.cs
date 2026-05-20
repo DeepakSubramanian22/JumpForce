aebed7c6875905202e4c92abf745969310796d95
Deeapk Subramanian <deepakpavan0418@gmail.com>
2026-05-20 16:27:53 +0530

    Added-Barrier

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,20 +5,28 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody playerRb;
 
     public float Jump = 20f; 
-    public Vector3 gravityModifier;
+    public float gravityModifier;
+
+    public bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
        playerRb = GetComponent<Rigidbody>();
-       Physics.gravity = gravityModifier;
+       Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
            playerRb.AddForce(Vector3.up * Jump , ForceMode.Impulse);
+           isGrounded = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
     }
 }
